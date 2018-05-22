41174a4a2231731675527acd86dc084986b50f9d
Stéphane Romany <sromany@excilys.com>
2018-05-22 17:57:47 +0200

    [FEAT] (Routes) Trick for redirecting in same url. See more for a convinient solution.

--- src/app/company-update/company-update.component.ts
+++ src/app/company-update/company-update.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Company} from "../company.model";
 import {FormBuilder, FormGroup , Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-company-update',
@@ -11,7 +11,7 @@ import {Router} from "@angular/router";
 
 export class CompanyUpdateComponent implements OnInit {
 
-  company: Company;
+  company: Company = new Company();
   mainForm: FormGroup;
 
   initForm() {
@@ -22,20 +22,29 @@ export class CompanyUpdateComponent implements OnInit {
     });
   }
 
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function(){return false;};
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        // trick the Router into believing it's last link wasn't previously loaded
+        this.router.navigated = false;
+        // if you need to scroll back to top, here is the right place
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 
   ngOnInit() {
     this.initForm();
-    this.company = new Company();
   }
 
   submit() : void {
     this.company.name = this.mainForm.get('name').value;
     this.company.description = this.mainForm.get('description').value;
     this.company.logo = this.mainForm.get('logo').value;
-    this.router.navigate(['update']).then((value: boolean) => value ? console.log('true') : console.log('false'));
     console.log(this.company);
     // Service.update().subscribe(() => console.log('Updated !'));
+    this.router.navigate(['update']).then((value: boolean) => value ? console.log('true') : console.log('false'));
   }
 
 }
