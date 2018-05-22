fa7a5f97a4c57fbb8f9f1c0e21bcc5705cfe8262
Samia Nabili <snabili@excilys.com>
2018-05-22 14:35:11 +0200

    [FEAT] add reactive validation to login form

--- src/app/app-login/app-login.component.ts
+++ src/app/app-login/app-login.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../auth.service';
 import {Router} from '@angular/router';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-app-login',
   templateUrl: './app-login.component.html',
   styleUrls: ['./app-login.component.scss']
 })
-export class AppLoginComponent {
+export class AppLoginComponent implements OnInit {
 
   message: string;
   username: string;
   password: string;
+  loginForm: FormGroup;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  ngOnInit() {
     this.setMessage();
+    this.createForm();
   }
 
   setMessage() {
     this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.minLength(2)],
+      password: ['', Validators.required]
+    });
   }
 
   login() {
