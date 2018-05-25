77f449a23b5c8f72a23e5a80f8cfe70b0feebe25
Samia Nabili <snabili@excilys.com>
2018-05-25 14:38:17 +0200

    [FIX] change expected request type for auth from json to text

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { tap, delay } from 'rxjs/operators';
-
+import { Observable } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  isLoggedIn = false;
-
   redirectUrl = '';
 
-  login(username: string, password: string): Observable<boolean> {
-    return of (true).pipe(delay(1000),
-      tap(val => {
-          if (username === 'admin' && password === 'admin') {
-            this.isLoggedIn = true;
-          }
-      })
-    );
+  baseUrl = 'http://localhost:8080/computer-database-webservice/';
+
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<string> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    // const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    const body =  { 'username' : username, 'password' : password }
+    return this.http.post(this.baseUrl + 'login', body, { headers: headers, responseType: 'text' });
   }
 
   logout(): void {
-    this.isLoggedIn = false;
+    console.log('AuthService logout removing token from localStorage');
+    localStorage.removeItem('token');
   }
+
+  isLoggedIn(): boolean {
+    console.log('Local Storage get token ', localStorage.getItem('token'))
+    console.log(localStorage.getItem('token') === null);
+    return !(localStorage.getItem('token') === null);
   }
+
+  getToken(): string{
+    return (localStorage.getItem('token'));
+  }
+}
 
 
